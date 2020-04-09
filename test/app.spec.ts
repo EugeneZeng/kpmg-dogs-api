86a4b5718bc44f086536bfff586300f41bdeb2eb
EugeneZeng <eugene@kobuhealth.com>
2020-04-09 18:28:10 +0800

    Added more test cases for images.

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -6,13 +6,41 @@ import { Suite } from "mocha";
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-describe('Get request', () => {
-    it('should go normally', () => chai.request(app).get('/').then(res => expect(res.text).to.equal('hello')));
-    it('get dog list should return sucess', (done) => {
+describe('Get dog breed list request', () => {
+    it('get dog breed list should return sucess', (done) => {
         chai.request(app).get('/list').then(res => {
             expect(res).to.have.status(200);
             expect(res).to.be.json;
-            expect(res.body.message).to.be.exist;
+            expect(res.body).to.be.exist;
+            expect(res.body).to.be.an('array').that.is.not.empty;
+            done();
+        }).catch(e => {
+            console.error(e);
+            done();
+        })
+    });
+});
+
+describe('Get dog breed images request', () => {
+    it('get dog breed images with breed only.', (done) => {
+        chai.request(app).get('/images/hound').then(res => {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.an('array').that.is.not.empty;
+            done();
+        }).catch(e => {
+            console.error(e);
+            done();
+        });
+    });
+    it('get dog breed images with breed and sub breed.', (done) => {
+        chai.request(app).get('/images/hound/afghan').then(res => {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.an('array').that.is.not.empty;
+            done();
+        }).catch(e => {
+            console.error(e);
             done();
         })
     });
